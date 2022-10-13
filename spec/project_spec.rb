a9ab606a739dd9d604098b45ba4608f02d8b706a
Anthony Gladden <a.gladden360@outlook.com>
2022-10-12 18:07:24 -0700

    updated Project test and added Project class

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -8,3 +8,10 @@ require('project')
     expect(project.title).to(eq("humpty dumpty"))
   end
 end
+
+  context "#id" do 
+  it "returns id before saving" do 
+    project = Project.new({:title => "humpty dumpty", :id => nil})
+    project.save()
+    expect(project.id).to(be_an_instance_of(Integer))
+  end
