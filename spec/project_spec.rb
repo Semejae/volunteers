d7b524f2bd0db956f4029b7198307d0970f54ab0
Anthony Gladden <a.gladden360@outlook.com>
2022-10-12 17:59:01 -0700

    created first test for project

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,2 +1,10 @@
 require('spec_helper')
 require('project')
+
+
+  describe Project do 
+    describe "#title" do
+    project = Project.new({:title => "humpty dumpty", :id => nil})
+    expect(project.title).to(eq("humpty dumpty"))
+  end
+end
