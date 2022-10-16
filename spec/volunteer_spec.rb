2a88fd1ca50486cef5d1a9c03a7530deb531bf8b
Anthony Gladden <a.gladden360@outlook.com>
2022-10-15 22:11:34 -0700

    created test for volunteer class

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -1,2 +1,16 @@
 require('spec_helper')
 require('volunteer')
+
+describe Volunteer do
+
+  before(:each) do 
+    @project = Project.new(:title => "humpty dumpty", :id => nil)
+    @project.save()
+  end
+
+describe "#name" do
+  it "returns volunteer name" do 
+    practice_volunteer = Volunteer.new({:volunteer => "Anthony", :project_id => 1, :id => nil})
+    expect(practice_volunteer.volunteer).to(eq("Anthony"))
+  end
+end
