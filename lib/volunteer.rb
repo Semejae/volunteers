7179799063e36def26080c2d9362ab75606204c5
Anthony Gladden <a.gladden360@outlook.com>
2022-10-16 15:34:30 -0700

    created method for volunteer_to_compare

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -11,3 +11,13 @@ class Volunteer
     @volunteer = attributes.fetch(:volunteer)
     @project_id = attributes.fetch(:project_id)
   end
+
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+  (self.volunteer() == volunteer_to_compare.volunteer()) && (self.project_id() == volunteer_to_compare.project_id())
+    else
+      false
+    end
+  end
+
+  
