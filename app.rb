3c645d9f730242982c20d60125987543f0595820
Anthony Gladden <a.gladden360@outlook.com>
2022-10-16 17:30:55 -0700

    updated views, app.rb and Created README

---
+++ app.rb
@@ -0,0 +1,78 @@
+require("pg")
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/project')
+require('pry')
+require('./lib/volunteer')
+also_reload('lib/**/*.rb')
+
+DB = PG.connect({:dbname => "volunteer_tracker"})  
+
+get('/') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+post('/projects') do
+  title = params[:project_name]
+  project = Project.new(:title => title, :id => nil)
+  project.save()
+  @projects = Project.all()
+  erb(:projects)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params)
+  @projects = Project.all
+  erb(:projects)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects/:id/volunteer') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new(params[:volunteer_name], @project.id, nil)
+  volunteer.save()
+  erb(:project)
+end
+
+patch('/projects/:id/volunteer/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:name], @project.id)
+  erb(:project)
+end
+
+
+delete('/projects/:id/volunteer/:volunteer_id') do
+
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
